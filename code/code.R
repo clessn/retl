635ed59d4334e73ace7981ef05c15690ce0b276f
Olivier Banville <oli.banville@gmail.com>
2022-05-09 10:46:15 -0400

    fix renv

--- code/code.R
+++ code/code.R
@@ -3,3 +3,4 @@ password <- Sys.getenv("clhub_password")
 app_token <- Sys.getenv("app_token")
 
 credentials <- hubr::get_credentials("https://clhub.clessn.cloud/", username, password)
+print(username)
